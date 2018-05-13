4c23b5406477a58ca711ed47d16ea2dd4319f9a8
vjgarciag96 <vjgarciag96@gmail.com>
2018-05-13 23:06:51 +0200

    Added recipes for patients

---
+++ DBScripts/AquilesDb_180513_213317.sql
@@ -0,0 +1,15 @@
+-- Receta [ent2]
+create table "public"."receta" (
+   "oid"  int4  not null,
+   "medicamento"  varchar(255),
+   "cantidad"  varchar(255),
+   "usos_diarios"  int4,
+  primary key ("oid")
+);
+
+
+-- Receta_Schedule [rel4]
+alter table "public"."receta"  add column  "schedule_oid"  int4;
+alter table "public"."receta"   add constraint fk_receta_schedule foreign key ("schedule_oid") references "public"."schedule" ("oid");
+
+
