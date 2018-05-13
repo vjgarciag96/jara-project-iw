291b3132cf58cfcd2e348e0781fd24470d8c51b6
vjgarciag96 <vjgarciag96@gmail.com>
2018-05-13 19:39:10 +0200

    Implemented first final version

---
+++ DBScripts/AquilesDb_180512_143013.sql
@@ -0,0 +1,9 @@
+-- TimeTableBase [ent1]
+create table "public"."timetablebase" (
+   "oid"  int4  not null,
+   "starttime"  varchar(255),
+   "day"  varchar(255),
+  primary key ("oid")
+);
+
+
---
+++ DBScripts/AquilesDb_180512_173446.sql
@@ -0,0 +1,5 @@
+-- User [User]
+alter table "public"."user"  add column  "nombre"  varchar(255);
+alter table "public"."user"  add column  "apellidos"  varchar(255);
+
+
